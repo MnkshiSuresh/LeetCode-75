c657b6477a10252032ae5e116eccf125bd960b8d
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-05-14 14:16:50 +0530

    Create Can Place Flowers.py

---
+++ Array,String/Can Place Flowers.py
@@ -0,0 +1,27 @@
+'''You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, 
+
+return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise. '''
+
+def planting(a,n):
+    count = 0
+    for i in range(len(a)):
+        if a[i]==1:
+            count += 1
+            for j in range(i+1,len(a)):
+                if a[j]==1:
+                    a[j]=0
+                    break;
+            
+    print(count)
+    if count>=n :
+        print("true")
+    else:
+        print("false")
+    
+    
+    
+a = [1,1,0,1,1,0,1,0,1,0,1]
+n=4
+planting(a,n)
