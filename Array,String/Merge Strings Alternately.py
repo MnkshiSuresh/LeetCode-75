308fe576f79809c8ae37dfb06677a2fb3a00ef6d
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-05-14 13:36:43 +0530

    Update Merge Strings Alternately.py

--- Array,String/Merge Strings Alternately.py
+++ Array,String/Merge Strings Alternately.py
@@ -1,5 +1,5 @@
-#You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1.
-#If a string is longer than the other, append the additional letters onto the end of the merged string.
+''' You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1.
+If a string is longer than the other, append the additional letters onto the end of the merged string. '''
 
 def mergestrings(word1, word2):
     merged = ""
