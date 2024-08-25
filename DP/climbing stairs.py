10b42519874bb09c95e8103790f5cf345837ecdf
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-08-25 09:10:03 +0530

    Update climbing stairs.py

--- DP/climbing stairs.py
+++ DP/climbing stairs.py
@@ -1,16 +1,10 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        n = len(nums)
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
         
-        if n == 1:
-            return nums[0]
-        
-        dp = [0] * n
-        
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        
-        for i in range(2, n):
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
-        
-        return dp[-1] 
+        return dp[-1]
